79a10924c74b87acc13a71b821bf887c2efc59a0
josefreitas2 <josefreitas2@gmail.com>
2016-11-05 16:50:10 +0000

    Add files via upload
    Add Free Shipping lable when product price greater than free-shipping treshold. -> Implementada informação de Envio grátis na página de produto quando o preço é superior ao valor mínimo para ter envio gratuito.
    Notice with minimum € reached to Free Shipping @ WooCommerce Cart -> Implementada informação de Envio grátis na Cart e Mini cart quando o valor mínimo é atingido.
    Notice with € remaining to Free Shipping @ WooCommerce Cart -> Implementada informação de Faltam X euros para ter envio grátis.

--- functions-05112016.php
+++ functions-05112016.php
@@ -100,3 +100,127 @@ echo '<div class="woocommerce-message-shipping"><img src="/wp-content/uploads/20
 }
  
 add_action( 'woocommerce_before_mini_cart', 'bbloomer_mini_cart_notice' );
+
+/**
+* @snippet Notice with minimum € reached to Free Shipping @ WooCommerce Cart
+*/
+ 
+function mjd_free_shipping_min_amount() {
+ 
+// Get Shipping Methods for Current Zone
+ 
+global $woocommerce;
+$shipping_methods = $woocommerce->shipping->get_shipping_methods();
+ 
+// Loop through the array to find min_amount value/s
+ 
+foreach($shipping_methods as $key => $value) {
+    if ( $shipping_methods[$key]->min_amount > 0 ) {
+      $min_amounts[$key] = $shipping_methods[$key]->min_amount;
+    }
+}
+ 
+if ( is_array($min_amounts) ) {
+ 
+// Find lowest min_amount
+ 
+$min_amount = min($min_amounts);
+ 
+// Get Cart Subtotal inc. Tax excl. Shipping
+ 
+$current = WC()->cart->subtotal;
+ 
+// If Subtotal > Min Amount Echo Notice
+ 
+if ( $current > $min_amount ) {
+echo '<div class="woocommerce-message-shipping"><img src="/wp-content/uploads/2016/11/envio-gratis.png">Envio grátis!</div>';
+}
+ 
+}
+ 
+}
+ 
+add_action( 'woocommerce_before_cart', 'mjd_free_shipping_min_amount' );
+
+/**
+* @snippet Notice with minimum € reached to Free Shipping @ WooCommerce Mini Cart
+*/
+
+function mjd_mini_cart_free_shipping_min_amount() {
+ 
+// Get Shipping Methods for Current Zone
+ 
+global $woocommerce;
+$shipping_methods = $woocommerce->shipping->get_shipping_methods();
+ 
+// Loop through the array to find min_amount value/s
+ 
+foreach($shipping_methods as $key => $value) {
+    if ( $shipping_methods[$key]->min_amount > 0 ) {
+      $min_amounts[$key] = $shipping_methods[$key]->min_amount;
+    }
+}
+ 
+if ( is_array($min_amounts) ) {
+ 
+// Find lowest min_amount
+ 
+$min_amount = min($min_amounts);
+ 
+// Get Cart Subtotal inc. Tax excl. Shipping
+ 
+$current = WC()->cart->subtotal;
+ 
+// If Subtotal > Min Amount Echo Notice
+ 
+if ( $current > $min_amount ) {
+echo '<div class="woocommerce-message-shipping"><img src="/wp-content/uploads/2016/11/envio-gratis.png">Envio grátis!</div>';
+}
+ 
+}
+ 
+}
+ 
+add_action( 'woocommerce_before_mini_cart', 'mjd_mini_cart_free_shipping_min_amount' );
+
+
+/**
+ * Add Free Shipping lable when product price greater than free-shipping treshold.
+ */
+function mjd_add_free_shipping() {
+
+    // Get Available Shipping Packages.
+    $packages = WC()->cart->get_shipping_packages();
+
+    $min_amount = false;
+
+    foreach ( $packages as $package ) {
+
+        $available_methods = WC()->shipping()->load_shipping_methods( $package );
+
+        foreach ( $available_methods as $method ) {
+            if ( 'free_shipping' === $method->id ) {
+
+                if ( 'min_amount' === $method->requires ) {
+                    $min_amount = $method->min_amount;
+                }
+            }
+        }
+    }
+
+    if ( ! $min_amount ) {
+        return;
+    }
+
+    // Get Product display price.
+    global $product;
+
+    $current = $product->get_display_price();
+
+    // If Subtotal > Min Amount Echo Notice.
+    if ( $current > $min_amount ) {
+        echo '<div class="woocommerce-message-shipping">' . esc_html__( 'Envio grátis!', '' ) . '</div>';
+    }
+
+}
+add_action( 'woocommerce_single_product_summary', 'mjd_add_free_shipping', 10 );
